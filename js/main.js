d50053cdde9bea1604f7595577f3c4b0ed9eea6e
margonaut <margaret.morgane@gmail.com>
2015-10-26 18:07:45 -0400

    Wrote JS methods to add player objects to Game

---
+++ js/main.js
@@ -0,0 +1,38 @@
+var Game = {
+  players: []
+}
+
+var players = Game.players;
+
+function Player(name, score){
+  this.name = name;
+  this.score = score;
+}
+
+function nameMatch(value) {
+  return value.name == this.name;
+}
+
+function playerExists(name) {
+  return players.some(nameMatch, {name: name});
+}
+
+function increaseScore(name, score) {
+  player = players.find(nameMatch, {name: name});
+  player.score += score;
+}
+
+$('#add').on("click", function(event) {
+  var name = $('input[name="name"]').val();
+  var score = parseInt($('input[name="score"]').val());
+  if (name) {
+    if (playerExists(name)) {
+      increaseScore(name, score);
+    } else {
+      newPlayer = new Player(name, score);
+      Game.players.push(newPlayer)
+    }
+  } else {
+    alert("Invalid input")
+  }
+});
